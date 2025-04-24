2a106aa5e3ee9d1c1f2d4cb4669457080cf24077
cvmdsp <55122455+cvmdsp@users.noreply.github.com>
2025-04-24 10:53:16 +0800

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,6 @@ import os
 
 
 def load_data(source_time_path, source_spectral_path, target_time_path, target_spectral_path):
-    # 初始化存储数据的列表
     source_time_data = []
     source_spectral_data = []
     target_time_data = []
@@ -13,10 +12,9 @@ def load_data(source_time_path, source_spectral_path, target_time_path, target_s
     source_labels = []
     target_labels = []
 
-    # 读取源域和目标域时间数据和光谱数据
+    
     for file_name in os.listdir(source_time_path):
         if file_name.endswith('.mat'):
-            # 加载每个.mat文件的数据
             mat_data = scipy.io.loadmat(os.path.join(source_time_path, file_name))
             source_time_data.append(mat_data['data'])
             source_labels.append(mat_data['arousal_labels'])
@@ -37,13 +35,13 @@ def load_data(source_time_path, source_spectral_path, target_time_path, target_s
             mat_data = scipy.io.loadmat(os.path.join(target_spectral_path, file_name))
             target_spectral_data.append(mat_data['data'])
 
-    # 将数据转换为 TensorFlow 张量
+    
     source_time_data = tf.convert_to_tensor(np.array(source_time_data), dtype=tf.float32)
     source_spectral_data = tf.convert_to_tensor(np.array(source_spectral_data), dtype=tf.float32)
     target_time_data = tf.convert_to_tensor(np.array(target_time_data), dtype=tf.float32)
     target_spectral_data = tf.convert_to_tensor(np.array(target_spectral_data), dtype=tf.float32)
 
-    # 将标签转换为 TensorFlow 张量
+    
     source_labels = tf.convert_to_tensor(np.array(source_labels), dtype=tf.int32)
     target_labels = tf.convert_to_tensor(np.array(target_labels), dtype=tf.int32)
 
